16e634d89f5408e6e50e85e5a2602ef17c4c2a0c
jnnjiang <jessica96106@163.com>
2019-03-18 18:15:01 +0800

    AndroidRapidDev: add module: tools
    Signed-off-by: jnnjiang <jessica96106@163.com>

--- app/src/main/java/com/nana/androidrapiddev/MainActivity.java
+++ app/src/main/java/com/nana/androidrapiddev/MainActivity.java
@@ -1,7 +1,20 @@
 package com.nana.androidrapiddev;
 
+import android.content.res.ColorStateList;
+import android.graphics.Bitmap;
+import android.graphics.PorterDuffColorFilter;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.RippleDrawable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.nana.tools.BitmapClip;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +22,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        FragmentManager fragmentManager = getSupportFragmentManager();
+//        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+//        Fragment fragment = new Fragment();
+//        fragmentTransaction.add(fragment,"no ui");
+//        finishAndRemoveTask();
+//        PorterDuffColorFilter
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v.setBackgroundTintList(getResources().getColorStateList(R.color.colorPrimary));
+            }
+        });
+
+//        button.setElevation(10f);
+//        button.setTranslationZ(20f);
+
+//        Button button1 = findViewById(R.id.button1);
+//        Drawable drawable = button1.getBackground();
+//        BitmapDrawable bd = (BitmapDrawable) drawable;
+//        Bitmap bitmap = BitmapClip.fillet(BitmapClip.ALL,bd.getBitmap(),8);
+//        button1.setBackground(new BitmapDrawable(getResources(),bitmap));
+//        RippleDrawable
     }
 }
